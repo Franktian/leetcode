ec9a56c50a7aeb9f4e82ce02729b35608dca9d47
Frank <tianyawen201209@hotmail.com>
2018-04-10 17:00:27 -0400

    Added remove element

--- removeDuplicates.py
+++ removeDuplicates.py
@@ -8,3 +8,19 @@ def removeDuplicates(nums):
             prev += 1
             nums[prev] = nums[i]
     return prev + 1
+
+def removeElement(nums, val):
+    if not nums:
+        return 0
+    if len(nums) == 1:
+        if nums[0] == val:
+            nums.pop()
+            return 0
+        else:
+            return 1
+    prev = 0
+    for i in range(len(nums)):
+        if nums[i] != val:
+            nums[prev] = nums[i]
+            prev += 1
+    return prev
