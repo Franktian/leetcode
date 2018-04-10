8966fbc148ebe073b23fd92f241f796781f3197a
Frank <tianyawen201209@hotmail.com>
2018-04-10 10:58:44 -0400

    Merge two lists

---
+++ mergeTwoLists.py
@@ -0,0 +1,35 @@
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def mergeTwoLists(l1, l2):
+    if l1 is None:
+        return l2
+    elif l2 is None:
+        return l1
+    elif l1.val < l2.val:
+        l1.next = mergeTwoLists(l1.next, l2)
+        return l1
+    else:
+        l2.next = mergeTwoLists(l1, l2.next)
+        return l2
+
+def mergeTwoListsItr(l1, l2):
+    head = ListNode(-1)
+    prev = head
+    
+    while l1 and l2:
+        if l1.val <= l2.val:
+            prev.next = l1
+            l1 = l1.next
+        else:
+            prev.next = l2
+            l2 = l2.next
+        prev = prev.next
+    if l1 is not None:
+        prev.next = l1
+    else:
+        prev.next = l2
+    return head.next
