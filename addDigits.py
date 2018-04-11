f0e4545cca84d227fd19fcdde1df330db00dd29c
Franktian <tianyawen201209@hotmail.com>
2018-04-11 00:04:57 -0400

    Added addDigits

---
+++ addDigits.py
@@ -0,0 +1,12 @@
+def addDigits(num):
+    if num < 10:
+        return num
+    s = sumOfDigit(num)
+    return addDigits(s)
+
+def sumOfDigit(num):
+    s = 0
+    while num != 0:
+        s += num % 10
+        num /= 10
+    return s
