eef5d66414b3d7a73d310081f3fef30847628f82
Frank <tianyawen201209@hotmail.com>
2018-04-17 12:50:50 -0400

    Added intersection 2

--- intersection.py
+++ intersection.py
@@ -14,3 +14,27 @@ def intersection(nums1, nums2):
             res.append(n)
             
     return res
+
+
+def intersection2(nums1, nums2):
+    ht1 = {}
+    ht2 = {}
+
+    for n in nums1:
+        if not ht1.get(n):
+            ht1[n] = 1
+        else:
+            ht1[n] += 1
+
+    res = []
+    for n in nums2:
+        if not ht2.get(n):
+            ht2[n] = 1
+        else:
+            ht2[n] += 1
+
+        if ht1.get(n) and ht2.get(n) <= ht1.get(n):
+            res.append(n)
+
+
+    return res
