d474e74a6c2ee1d18a73d5f2873904d426935563
Frank <tianyawen201209@hotmail.com>
2018-04-20 16:32:53 -0400

    Added minimum climbing stairs

--- maxSubArray.py
+++ maxSubArray.py
@@ -1,7 +1,21 @@
-def maxSubArray(nums):
-    current_sum = nums[0]
-    max_sum = nums[0]
+import sys
 
-    for i in nums:
-        current_sum += i
-        print current_sum
+def maxSubArray(A):
+    # REDO this
+    curr = mxm = A[0]
+    
+    for num in A[1:]:
+        curr = max(num, num + curr)
+        mxm = max(mxm, curr)
+    return mxm
+
+def minCostClimbingStairs(cost):
+    f1 = cost[0]
+    f2 = cost[1]
+
+    for n in cost[2:]:
+        res = min(n + f1, n + f2)
+        f1 = f2
+        f2 = res
+
+    return min(f1, f2)
