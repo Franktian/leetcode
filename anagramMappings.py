5b7a61d36548b94eed82a7e4fe8463a331318b9a
Franktian <tianyawen201209@hotmail.com>
2018-04-21 17:10:14 -0400

    Added anagram mappings

---
+++ anagramMappings.py
@@ -0,0 +1,12 @@
+def anagramMappings(A, B):
+    ht = {}
+    
+    for i in range(len(B)):
+        ht[B[i]] = i
+    
+    res = []
+    
+    for n in A:
+        res.append(ht.get(n))
+        
+    return res
