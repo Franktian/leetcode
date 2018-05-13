0a760e3f6751c8893edb0858f10a5d39aa975f3d
Franktian <tianyawen201209@hotmail.com>
2018-05-13 17:33:22 -0400

    Added two pointer method for has cycle

--- hasCycle.py
+++ hasCycle.py
@@ -9,3 +9,18 @@ def hasCycle(head):
         head = head.next
 
     return False
+
+def hasCycleTwoPointer(head):
+    if head == None or head.next == None:
+        return False
+
+    slow = head
+    fast = head.next
+
+    while slow != fast:
+        if fast == None or fast.next == None:
+            return False
+        slow = slow.next
+        fast = fast.next.next
+
+    return True
