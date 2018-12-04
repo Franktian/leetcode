d402b1ebc6d47d3aaed713bcf8e15f4b637036b4
Frank Tian <frank.tian@accedo.tv>
2018-12-04 11:41:11 -0500

    Added isomorphic and group isomorphic

---
+++ isIsomorphic.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        mapS = {}
+        mapT = {}
+
+        for i in range(len(s)):
+            if not s[i] in mapS:
+                mapS[s[i]] = i
+            
+            if not t[i] in mapT:
+                mapT[t[i]] = i
+
+            if mapS[s[i]] != mapT[t[i]]:
+                return False
+
+        return True
+
+
+def groupIsomorphic(strs):
+    import collections
+    res = collections.defaultdict(list)
+
+    for s in strs:
+        ht = {}
+        idx = ''
+        
+        for i in range(len(s)):
+            if not s[i] in ht:
+                ht[s[i]] = i
+            idx += str(ht[s[i]])
+        res[idx].append(s)
+
+    return res.values()
