7efe9a629f6592f7be2e2d77c5b7c82f62051c1e
Franktian <tianyawen201209@hotmail.com>
2018-12-04 23:10:24 -0500

    Updated threeSum

--- threeSum.py
+++ threeSum.py
@@ -1,44 +1,26 @@
-def threeSum(nums):
-    nums.sort()
-    res = set()
-    
-    for k in range(len(nums)):
-        target = -nums[k]
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        res = set()
 
-        i = k + 1
-        j = len(nums) - 1
+        for k in range(len(nums)):
+            i = k + 1
+            j = len(nums) - 1
 
-        while i < j:
-            if nums[i] + nums[j] == target:
-                res.add((nums[k], nums[i], nums[j]))
-                i += 1
-                j -= 1
-            elif nums[i] + nums[j] > 0:
-                j -= 1
-            else:
-                i += 1
-    return map(list, res)
+            while i < j:
+                s = nums[i] + nums[j] + nums[k]
 
-def solution(A, B):
-    
-    wins = 0
-    for i in range(len(A)):
-        card_a = A[i]
-        card_b = B[i]
-        if check_win(card_a, card_b):
-            wins += 1
-    return wins
+                if s == 0:
+                    res.add((nums[i], nums[j], nums[k]))
+                    i += 1
+                    j -= 1
+                elif s > 0:
+                    j -= 1
+                else:
+                    i += 1
 
-def check_win(card_a, card_b):
-    if card_a == 'A':
-        return card_b != card_a
-    if card_a == 'K':
-        return card_b not in 'AK'
-    if card_a == 'Q':
-        return card_b not in 'AKQ'
-    if card_a == 'J':
-        return card_b not in 'AKQJ'
-    if card_a == 'T':
-        return card_b not in 'AKQJT'
-
-    return card_a > card_b
+        return map(list, res)
