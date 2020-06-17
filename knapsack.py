b73ec8b8d16784b1f0d15f46a9d4e1c3fc278def
Frank Tian <tianyawen201209@hotmail.com>
2020-06-17 19:00:13 -0400

    Added knapsack

---
+++ knapsack.py
@@ -0,0 +1,7 @@
+def solve_knapsack(profits, weights, capacity):
+  return -1
+
+
+if __name__ == '__main__':
+  assert solve_knapsack([1, 6, 10, 16], [1, 2, 3, 5], 7) == 22
+  assert solve_knapsack([1, 6, 10, 16], [1, 2, 3, 5], 6) == 17
